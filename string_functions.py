4a88bcc94b2ae69cfcd14733818420898e3bb89e
Yi Shuen Lim <yish@yishs-laptop.local>
2018-10-24 11:03:16 -0400

    added functions

--- string_functions.py
+++ string_functions.py
@@ -1,33 +1,69 @@
 def say_hello(name):
+    n = str(name)
+    return "Hi my name is {}".format(n)
     # takes in a name and returns the string "Hi my name is " plus the name
     # use whichever form of interpolation is most appropriate
-    pass
+
 
 def replace_given_substring(str_to_replace, str_to_insert, string):
+    return string.replace(str_to_replace, str_to_insert)
     # this function takes three parameters --
     # the first is the substring we would like to replace.
     # the second substring is what we would like to use inplace of the first
     # the third is the actual string which we want to operate on
     # the function should return the new string
-    pass
+
 
 def remove_duplicate_punctuation(string):
+    filtered = ""
+    from string import punctuation
+    punc = set(punctuation)
+    for k in range(len(string)-1):
+        if string[k] not in punc:
+            filtered += string[k]
+        if string[k] in punc:
+            if string[k] != string[k+1]:
+                filtered += string[k]
+            else:
+                filtered += ""
+    return filtered + string[-1]
     # should remove all duplicate punctuation marks in a given string
     # i.e. "Hi!!!!!!" should be reformatted to "Hi!"
     # i.e. "Hello..... My name is Terrance!! How are you???" -> "Hello. My name is Terrance! How are you?"
-    pass
 
+
+def atdotcom(x):
+    if "@" and ".com" in x:
+        return True
+    else:
+        return False
+    
+def nospchx(x):
+    wo_spchx = []
+    special_chx = ['*','~','#','$','%','&','(',')','\`','\"',':',';','/','>','<', '!']
+    for chx in x:
+        if chx not in special_chx:
+            wo_spchx.append(chx)
+    return wo_spchx
 
 def validate_email_format(email):
-    # should make sure there are no special characters (i.e. *,~,#,$,%,&,(,),`,",',:,;,/,>,<)
+    if atdotcom(email) == True and nospchx(email) == list(email):
+        return True
+    else:
+        return False
     # make sure the email contains an @ symbol and a .com
     # return True if format passes tests, return False otherwise
-    pass
 
 
 def anonymize_credit_card_number(credit_card_number):
-    # should replace all characters except the last 4 with 'X'
-    # return the anonymized credit card number as a string
-    # the credit card may have characters that are not numbers (i.e. spaces and dashes, which we would want to keep)
-    # i.e. 1234-5678-90-1234 -> XXXX-XXXX-XX-1234
-    pass
+    ccno = credit_card_number
+    num_length = len(list(ccno))
+    digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    masked = ""
+    for x in ccno[0:(num_length - 4)]:
+        if x in digits:
+            masked +=  "X"
+        else:
+            masked += (str(x))
+    return masked + ccno[-4:]
+        
